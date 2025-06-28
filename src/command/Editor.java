e4a668dc19fd8a83479db7c339b02d16d5090beb
snehasishroy <snehasishroy39@gmail.com>
2025-06-28 13:34:16 +0530

    Update Command Design Pattern

--- src/command/Editor.java
+++ src/command/Editor.java
@@ -6,6 +6,8 @@ public class Editor {
     int c;
 
     public void execute(Command command) {
+        // this is wrong - this acts as a strategy pattern in which the editor is a simple pass through
+        // correct way is the sender invoking the command object which manipulates the receiver
         command.execute();
     }
 
